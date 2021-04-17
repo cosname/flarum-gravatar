fa9464e40ef864fc131c9c017d03aaa36f3b2922
Yixuan Qiu <yixuanq@gmail.com>
2021-04-17 18:56:42 +0800

    new extender

--- extend.php
+++ extend.php
@@ -1,16 +1,24 @@
 <?php
 
 use Flarum\Extend;
-use Cosname\Listener;
-use Illuminate\Contracts\Events\Dispatcher;
+use Flarum\Api\Serializer\BasicUserSerializer;
 
 return [
     // Load JS script
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js'),
     
-    // Add listener
-    function (Dispatcher $events) {
-        $events->subscribe(Listener\AddUserEmailAttributes::class);
-    }
+    // Add user email
+    //
+    // In the discussion list, by default the JS frontend only contains the 'username'
+    // and 'avatarUrl' attributes of users. Therefore, we cannot calculate email
+    // hash to obtain the Gravatar link. This extendor adds the 'email' attribute
+    // to the user so that we can get access to the email address in the frontend.
+    //
+    // See https://github.com/flarum/core/blob/master/tests/integration/extenders/ApiSerializerTest.php
+    // and https://github.com/flarum/core/blob/master/src/Extend/ApiSerializer.php
+    (new Extend\ApiSerializer(BasicUserSerializer::class))
+        ->attribute('email', function($serializer, $model, $attributes) {
+            return $model->email;
+        })
 ];
